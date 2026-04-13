04a059005782a82ac134d2ce44670a7c9be88c17
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2026-04-13 14:30:02 -0300

    feat: Inicio estruturação de Funcionarios.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import FuncionarioView from '@/views/FuncionarioView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+    path: '/funcionarios',
+    name: 'funcionarios',
+    component: () => import('../views/FuncionarioView.vue')
+    }
+  ],
 })
 
 export default router
---
+++ src/views/FuncionarioView.vue
@@ -0,0 +1,7 @@
+<script>
+</script>
+<template>
+    <h1>Funcionários</h1>
+    <h2>Informe-se sobre os Funcionários e suas ocupações.</h2>
+    <button>+ Adicionar evento</button>
+</template>
