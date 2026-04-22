440b07d026aa57f64dae43de6b0a6fa76847fcee
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2026-04-22 15:17:12 -0300

    feat: estrutura de FuncionarioComponent

--- src/views/FuncionarioView.vue
+++ src/views/FuncionarioView.vue
@@ -1,7 +1,9 @@
-<script>
+<script setup>
+import FuncionarioComponent from '../components/FuncionarioComponent.vue';
 </script>
 <template>
     <h1>Funcionários</h1>
     <h2>Informe-se sobre os Funcionários e suas ocupações.</h2>
-    <button>+ Adicionar evento</button>
+    <button>+ Adicionar Funcionário</button>
+    <FuncionarioComponent />
 </template>
